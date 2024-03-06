11f950b6c3556f2ab8d5571bab1e81b624432bf8
Mariya Tulina <mariya.tulina@nexign.com>
2024-03-06 22:21:22 +0400

    выделена фикстура для тестов групп и контактов, осуществлен перевод на py.test

--- contact.py
+++ contact.py
@@ -1,8 +1,4 @@
-class Group:
-    def __init__(self, name, header, footer):
-        self.name = name
-        self.header = header
-        self.footer = footer
+
 
 class Contact:
 
--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
-from models import Group
+from group import Group
 from application import Application
 
 
