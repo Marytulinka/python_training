bd75380f0e5bc0cc3515b1bcd7927a421c3a8433
Mariya Tulina <mariya.tulina@nexign.com>
2024-03-06 23:48:33 +0400

    Реализована двухуровневую архитектуру тестов: Созданы классы-помощники для управления сессией, для работы с группами, для работы с контактами. В них перенесены вспомогательные функции. Создать класс-менеджер, который инициализирует всех помощников

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,8 +13,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login("admin", "secret")
-    app.create_group(Group("тест", "тест", "тест"))
+    app.group.create(Group("тест", "тест", "тест"))
     app.session.logout()
 
+def test_add_empty_group(app):
+    app.session.login("admin", "secret")
+    app.group.create(Group("", "", ""))
+    app.session.logout()
 
 
