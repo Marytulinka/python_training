4ffc4e11f61dc23e2369e9d001ca6cc2f56f50ac
Mariya Tulina <mariya.tulina@nexign.com>
2024-03-14 23:37:57 +0400

    Реализована "интеллектуальная" фикстуры: валидация фикстуры перед каждым тестом

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from models.group import Group
-import time
 
 
 def test_add_group(app):
     app.group.create(Group("тест", "тест", "тест"))
-    time.sleep(3)
+
 
 def test_add_empty_group(app):
     app.group.create(Group("", "", ""))
