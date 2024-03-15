056fe3c81b03099ea9e1be9bcae63e20631e220c
Mariya Tulina <mariya.tulina@nexign.com>
2024-03-15 22:07:05 +0400

    Доработаны тесты для удаления и модификации групп: они предварительно проверяют наличие хотя бы одной группы, и если группы нет -- предварительно создают её.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-import time
+from models.group import Group
 
 
 def test_delete_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.delete_first_group()
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,11 +4,14 @@ import time
 
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
     time.sleep(3)
 
 
 def test_modify_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
-
 
