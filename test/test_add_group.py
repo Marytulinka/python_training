b823e57a344dcd4c293f67a391da96318f9f8a54
Mariya Tulina <mariya.tulina@nexign.com>
2024-03-18 23:31:24 +0400

    Загружаем информацию из тестируемого приложения в виде списков. Список групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,11 +3,17 @@ from models.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group("тест", "тест", "тест"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group("", "", ""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 
