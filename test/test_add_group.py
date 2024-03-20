014e98bc8116c7dafbccefcd8ba4b37cc19c0eb5
Mariya Tulina <mariya.tulina@nexign.com>
2024-03-20 22:52:24 +0400

    Сортировка списков групп перед сравнением

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,16 +4,22 @@ from models.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("тест", "тест", "тест"))
+    group = Group("тест", "тест", "тест")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("", "", ""))
+    group = Group("", "", "")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
